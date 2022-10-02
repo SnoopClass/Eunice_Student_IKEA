734ae38491e2a316110d256dd9088f8abbfe7b8e
EuniceNam <caellime@gmail.com>
2022-10-03 00:09:29 +0900

    [FIX] 탭바에서 구현하지 않을 탭뷰를 수정
    구현하지 않을 탭뷰에 들어간 NavigationController를 지우고, 축약형 인수(shorthand argument names)를 사용해 모양새를 바꿨습니다.

--- SnoopClass-IKEA/TabBarController.swift
+++ SnoopClass-IKEA/TabBarController.swift
@@ -18,31 +18,32 @@ enum TabIcon: String {
 
 class TabBarController: UITabBarController {
     // 탭바의 각 탭
-    private lazy var homeViewController: UINavigationController = {
-        let viewController = OtherTabViewController()
-        viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.home.rawValue), tag: 0)
-        return UINavigationController(rootViewController: viewController)
-    }()
+    private lazy var homeViewController: UIViewController = {
+        $0.view.backgroundColor = .systemBrown
+        $0.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.home.rawValue), tag: 0)
+        return $0
+    }(UIViewController())
+    // 상품 상세페이지로 이동시 네비게이션컨트롤러를 사용하므로 미리 넣음
     private lazy var searchViewController: UINavigationController = {
         let viewController = SearchViewController()
         viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.search.rawValue), tag: 1)
         return UINavigationController(rootViewController: viewController)
     }()
-    private lazy var userViewController: UINavigationController = {
-        let viewController = OtherTabViewController()
-        viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.user.rawValue), tag: 2)
-        return UINavigationController(rootViewController: viewController)
-    }()
-    private lazy var wishViewController: UINavigationController = {
-        let viewController = OtherTabViewController()
-        viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.wish.rawValue), tag: 3)
-        return UINavigationController(rootViewController: viewController)
-    }()
-    private lazy var cartViewController: UINavigationController = {
-        let viewController = OtherTabViewController()
-        viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.cart.rawValue), tag: 4)
-        return UINavigationController(rootViewController: viewController)
-    }()
+    private lazy var userViewController: UIViewController = {
+        $0.view.backgroundColor = .systemMint
+        $0.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.user.rawValue), tag: 2)
+        return $0
+    }(UIViewController())
+    private lazy var wishViewController: UIViewController = {
+        $0.view.backgroundColor = .systemPink
+        $0.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.wish.rawValue), tag: 3)
+        return $0
+    }(UIViewController())
+    private lazy var cartViewController: UIViewController = {
+        $0.view.backgroundColor = .systemGreen
+        $0.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: TabIcon.cart.rawValue), tag: 4)
+        return $0
+    }(UIViewController())
 
     override func viewDidLoad() {
         super.viewDidLoad()
